6d28b0449e3438b975ca753d76477b395b38d938
MAB <mab@mab.net>
2018-07-30 06:58:49 -0700

    display time now

--- Alarm.ino
+++ Alarm.ino
@@ -13,6 +13,7 @@ const int ledPin    = 12;
 const int buttonPin = 2;
 
 byte increment = 0;
+bool a, b;
 
 CRGB led[numLed];
 
@@ -43,17 +44,31 @@ void loop() {
     while (!buttonPressed()) {}
     delay(1000);
   }
+
+  displayTime();
   
-  increment = Clock.getSecond()*4;
-  bool a,b;
-  for (int i = 0; i < numLed; i++) {
-    led[i] = CHSV(Clock.getHour(a, b)*10, Clock.getMinute()*4, increment);
-    increment += 5;
-  }
   FastLED.show();
   FastLED.delay(1000);
 }
 
 boolean buttonPressed() {
   return digitalRead(buttonPin) == LOW;
+}
+
+//sadly can't display full min and second over 5 bits ...
+void displayTime() {
+  FastLED.clear(true);
+  CRGB color = ColorFromPalette( RainbowColors_p, Clock.getDate(), 255, LINEARBLEND);
+  byte data[3];
+  data[2] = Clock.getHour(a, b);
+  data[1] = Clock.getMinute();
+  data[0] = Clock.getSecond();
+  for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 5; i++) {
+      if(data[j] & 0x01){
+        led[i+j*5] = color;
+      }
+      data[j] = data[j] >> 1;
+    }
+  }
 }
