8553afd7a8082af4d2aa2704d03152a0413fc182
MAB <mab@mab.net>
2018-07-31 03:34:57 -0700

    created sunrise sequence, programmed it to trigger at 5h27, also added time serial output when program start, as a sanity check

--- Alarm.ino
+++ Alarm.ino
@@ -12,6 +12,10 @@ const int numLed    = 15;
 const int ledPin    = 12;
 const int buttonPin = 2;
 
+int alarmH = 5;
+int alarmM = 27;
+int divider = 1;
+
 byte increment = 0;
 bool a, b;
 
@@ -33,10 +37,77 @@ void setup() {
 
   // Start the I2C interface
   Wire.begin();
-
+  
+  FastLED.clear(true);
+  
+  Serial.begin(9600);
+  Serial.print(Clock.getHour(a, b)); Serial.print(":"); Serial.println(Clock.getMinute());
+  Serial.end();
+  //alarmSequence();
 }
 
 void loop() {
+  //wait for time to start
+  int hour = Clock.getHour(a, b);
+  int minute = Clock.getMinute();
+  if (hour == alarmH && minute == alarmM) {
+    alarmSequence();
+    while (hour == alarmH && minute == alarmM) {
+      minute = Clock.getMinute();
+      delay(10000);
+    }
+  }
+}
+
+void alarmSequence() {
+
+  //turn all led a pale blue 1.5min
+  for (int i = 0 ; i < 15; i++) {
+    led[i] = CRGB(0, 0, 1);
+    FastLED.show();
+    FastLED.delay(10000 / divider);
+  }
+
+  //then make it red 2min
+  for (int j = 1; j < 10; j++) {
+    for (int i = 0 ; i < 15; i++) {
+      led[i] = CRGB(j << 1, 0, 2 - min(2, j));
+      FastLED.show();
+      FastLED.delay(800 / divider);
+    }
+  }
+
+  //then gold 2min
+  for (int j = 1; j < 10; j++) {
+    for (int i = 0 ; i < 15; i++) {
+      led[i] = CRGB(20 + j << 1, j << 2, 0);
+      FastLED.show();
+      FastLED.delay(800 / divider);
+    }
+  }
+
+  //then white 2min
+  for (int j = 1; j < 40; j++) {
+    for (int i = 0 ; i < 15; i++) {
+      led[i] = CRGB(40 + j, 40 + j, j << 1);
+      FastLED.show();
+      FastLED.delay(200 / divider);
+    }
+  }
+
+  //finally, rainbow! 5min
+  for (int j = 1; j < 3000; j++) {
+    for (int i = 0 ; i < 15; i++) {
+      led[i] = ColorFromPalette( RainbowColors_p, j+i*5, 128, LINEARBLEND);;
+      FastLED.show();
+    }
+    FastLED.delay(100 / divider);
+  }
+  
+  FastLED.clear(true);
+  
+
+/*
   // put your main code here, to run repeatedly:
   if (buttonPressed()) {
     FastLED.clear(true);
@@ -45,10 +116,11 @@ void loop() {
     delay(1000);
   }
 
-  displayTime();
-  
+  // displayTime();
+
   FastLED.show();
   FastLED.delay(1000);
+  */
 }
 
 boolean buttonPressed() {
@@ -65,8 +137,8 @@ void displayTime() {
   data[0] = Clock.getSecond();
   for (int j = 0; j < 3; j++) {
     for (int i = 0; i < 5; i++) {
-      if(data[j] & 0x01){
-        led[i+j*5] = color;
+      if (data[j] & 0x01) {
+        led[i + j * 5] = color;
       }
       data[j] = data[j] >> 1;
     }
